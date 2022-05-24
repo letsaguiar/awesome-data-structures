2d2c7967f43223b87726758cda09562d3d82ca24
Letícia Aguiar <letsaguiar@yandex.com>
2022-05-24 15:35:27 -0300

    Create AbstractLinkedList

---
+++ app/LinkedList/AbstractLinkedList.ts
@@ -0,0 +1,37 @@
+import { ListNode } from "./ListNode"
+
+export class AbstractLinkedList {
+    public head: ListNode
+    public tail: ListNode
+    public size: number
+
+    constructor () {
+        this.head = null
+        this.tail = null
+        this.size = 0
+    }
+
+    incrementSize () {
+        this.size += 1
+    }
+
+    decrementSize () {
+        this.size -= 1
+    }
+
+    addToFront (data: any) : ListNode {
+        throw new Error('Method not implemented')
+    }
+
+    addToBack (data: any) : ListNode {
+        throw new Error('Method not implemented')
+    }
+
+    removeFromFront () : ListNode {
+        throw new Error('Method not implemented')
+    }
+
+    removeFromBack () : ListNode {
+        throw new Error('Method not implemented')
+    }
+}
---
+++ app/LinkedList/ListNode.ts
@@ -0,0 +1,11 @@
+export class ListNode {
+    data: any
+    next: ListNode
+    prev: ListNode
+
+    constructor (data: any) {
+        this.data = data
+        this.next = null
+        this.prev = null
+    }
+}
