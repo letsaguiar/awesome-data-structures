69b331df2216631c8d17db9b8d95c1aa6d825a33
Letícia Aguiar <letsaguiar@yandex.com>
2022-05-29 08:59:12 -0300

    Create DoublyLinkedList

---
+++ app/LinkedList/DoublyLinkedList.ts
@@ -0,0 +1,27 @@
+import { AbstractLinkedList } from "./AbstractLinkedList";
+
+export class DoublyLinkedList extends AbstractLinkedList {
+    addToFront(data: any): void {
+        
+    }
+
+    addToBack(data: any): void {
+        
+    }
+
+    insertAt(data: any, index: number): void {
+        
+    }
+
+    removeFromFront() {
+        
+    }
+
+    removeFromBack() {
+        
+    }
+
+    deleteAt(index: number) {
+        
+    }
+}
