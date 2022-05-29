cbec05382f0a4b4302fe711e56627f4521847ca3
Letícia Aguiar <letsaguiar@yandex.com>
2022-05-29 09:28:12 -0300

    Create CircularLinkedList

---
+++ app/LinkedList/CircularLinkedList.ts
@@ -0,0 +1,36 @@
+import { AbstractLinkedList } from "./AbstractLinkedList";
+import { ListNode } from "./ListNode";
+
+export class CircularLinkedList extends AbstractLinkedList {
+    addToFront(data: any): void {
+        
+    }
+
+    addToBack(data: any): void {
+        
+    }
+
+    private rInsertAt (data: any, index: number): void {
+
+    }
+
+    insertAt (data: any, index: number): void {
+        
+    }
+
+    removeFromFront () {
+        
+    }
+
+    removeFromBack () {
+        
+    }
+
+    private rDeleteAt (index: number) {
+        
+    }
+
+    deleteAt (index: number) {
+        
+    }
+}
