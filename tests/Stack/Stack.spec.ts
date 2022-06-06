7017d41f9f5c4c0e31a790011d99e2c186afe9e9
Letícia Aguiar <letsaguiar@yandex.com>
2022-06-06 05:49:52 -0300

    Tests new funcs

--- tests/Stack/Stack.spec.ts
+++ tests/Stack/Stack.spec.ts
@@ -12,13 +12,15 @@ beforeEach(() => {
     full_stack.push(3)
 })
 
-describe('stack operations', () => {
-    test('create an empty stack', () => {
+describe('creates an empty stack', () => {
+    test('creates an empty stack', () => {
         expect(empty_stack).toBeDefined()
         expect(empty_stack.size).toBe(0)
         expect(empty_stack.begin).toBe(null)
     })
+})
 
+describe('add operations', () => {
     test('push one element', () => {
         empty_stack.push(1)
         expect(empty_stack.size).toBe(1)
@@ -28,8 +30,10 @@ describe('stack operations', () => {
         expect(full_stack.size).toBe(4)
         expect(full_stack.begin).toBe(4)
     })
+})
 
-    test('Pop an empty stack', () => {
+describe('remove operations', () => {
+    test('pop an empty stack', () => {
         expect(() => {
             empty_stack.pop()
         }).toThrowError()
@@ -41,9 +45,35 @@ describe('stack operations', () => {
         expect(full_stack.size).toBe(2)
         expect(full_stack.begin).toBe(2)
     })
+})
 
-    test('peek the first element', () => {
+describe('clear operations', () => {
+    test('clear a stack', () => {
+        full_stack.clear()
+
+        expect(full_stack.begin).toBe(null)
+        expect(full_stack.size).toBe(0)
+    })
+
+    test('check if a stack is empty', () => {
+        expect(full_stack.isEmpty()).toBe(false)
+        expect(empty_stack.isEmpty()).toBe(true)
+    })
+})
+
+describe('getters and setters', () => {
+    test('begin', () => {
         expect(full_stack.begin).toBe(3)
         expect(empty_stack.begin).toBe(null)
+    })
+
+    test('end', () => {
+        expect(full_stack.end).toBe(1)
+        expect(empty_stack.begin).toBe(null)
+    })
+
+    test('size', () => {
+        expect(full_stack.size).toBe(3)
+        expect(empty_stack.size).toBe(0)
     })
 })
